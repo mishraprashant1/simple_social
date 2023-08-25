29d47b23f7b861d95a794c23d2613bd1a24c8681
Prashant Mishra <prashant.mishra@tophire.com>
2023-08-25 18:54:16 +0530

    Updated SoftDeleteModel

--- apps/core/models/base.py
+++ apps/core/models/base.py
@@ -10,8 +10,43 @@ class TimeStampedModel(models.Model):
         abstract = True
 
 
+class SoftDeleteQuerySet(models.QuerySet):
+    def delete(self):
+        return super().update(deleted_at=timezone.now())
+
+    def hard_delete(self):
+        return super().delete()
+
+    def alive(self):
+        return self.filter(deleted_at=None)
+
+    def dead(self):
+        return self.exclude(deleted_at=None)
+
+    def all(self, *args, **kwargs):
+        kwargs['deleted_at__isnull'] = True
+        return super().filter(*args, **kwargs)
+
+    def filter(self, *args, **kwargs):
+        if 'deleted_at__isnull' not in kwargs:
+            kwargs['deleted_at__isnull'] = True
+        return super().filter(*args, **kwargs)
+
+
+class SoftDeleteManager(models.Manager):
+    def get_queryset(self):
+        return SoftDeleteQuerySet(self.model, using=self._db)
+
+    def all(self, *args, **kwargs):
+        kwargs['deleted_at__isnull'] = True
+        return self.get_queryset().filter(*args, **kwargs)
+
+
 class SoftDeleteModel(TimeStampedModel):
     deleted_at = models.DateTimeField(null=True, blank=True)
+
+    objects = SoftDeleteManager()
+    all_objects = models.Manager()
 
     def delete(self, using=None, keep_parents=False):
         self.deleted_at = timezone.now()
