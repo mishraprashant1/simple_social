ef985e0a3bf5c8da2830e4accd79fa537e9bbdb4
Prashant Mishra <prashant.mishra@tophire.com>
2023-11-25 10:41:38 +0530

    Minor change

--- apps/retrieve_post/urls/__init__.py
+++ apps/retrieve_post/urls/__init__.py
@@ -3,5 +3,5 @@ from apps.retrieve_post.api.post_by_user import PostByUserView, GetTimelineView
 
 urlpatterns = [
     path('api/post_by_user/<uuid:user_uuid>', PostByUserView.as_view(), name='post_by_user'),
-    path('api/get_timeline', GetTimelineView.as_view(), name='get_timeline'),
+    path('api/timeline', GetTimelineView.as_view(), name='user_timeline'),
 ]
