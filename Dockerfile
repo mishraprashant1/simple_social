a8a075dc5de3065e6f3e1d42f47d6655dfd8edee
Prashant <prashant.mishra@tophire.co>
2024-07-17 18:10:05 +0530

    Docker setup related changes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.11
+FROM python:3.11-slim as base
 
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
@@ -13,6 +13,16 @@ RUN poetry install --no-root
 
 COPY ./simple_social /app/
 
+CMD bash "poetry run python manage.py migrate"
+
+FROM base as development
+
 EXPOSE 8000
 
 CMD ["poetry", "run", "python", "manage.py", "runserver", "0.0.0.0:8000"]
+
+FROM base as production
+
+EXPOSE 8000
+
+CMD ["poetry", "run", "gunicorn", "simple_social.wsgi:application", "--bind", "0.0.0.0:8000"]
--- apps/user/urls.py
+++ apps/user/urls.py
@@ -7,8 +7,8 @@ from rest_framework_simplejwt.views import (
 from apps.user.api.signup import SignUpView
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/signin/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/signin/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/signin/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/signup/', SignUpView.as_view(), name='signup'),
 ]
